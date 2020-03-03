b955e939d2d6751a4a3bb0f92b3d0f90918808db
Pickleses <43190448+Pickleses@users.noreply.github.com>
2020-03-03 11:41:37 -0800

    Added button and other bug fixes

--- sketch.js
+++ sketch.js
@@ -1,39 +1,70 @@
-let fullPara = "Saw that you were trying to rent your apartment. I am the assistant to the of owner Boston Z Realty, Dillon Currier. He has 19 years experience and a 5-star rating from all his previous clients. The tenants pay our fee, so we wouldn’t charge you anything and we would do everything to get the apartment rented: Marketing on over 60 websites and social media, qualifying tenants, showing the unit, processing paperwork, etc. Plus you can keep your ads up if you want. Can you do a quick call with him to discuss?"
 
-
+let button1, button2, button3, button4, button5;
+let newSent1, newSent2, newSent3, newSent4, newSent5;
 
 
 
 function setup() {
   //createCanvas(windowWidth, windowHeight - 50);
-//  background(51);
+  //background(51);
 
-  let rand = floor(random(0, 2));
+//  let rand = floor(random(0, 4)), 2));
 
-  let newSent1 = firstSent[floor(random(0, 3))] + " " + secondSent[floor(random(0, 3))] + " " + thirdSent[floor(random(0, 3))] + " " + fourthSent[floor(random(0, 3))] + " " + fifthSent[floor(random(0, 3))] + " " + sixthSent[floor(random(0, 3))] + " " + seventhSent[floor(random(0, 3))] + eigthSent[floor(random(0,3))];
-  let newSent2 = firstSent[floor(random(0, 3))] + " " + secondSent[floor(random(0, 3))] + " " + thirdSent[floor(random(0, 3))] + " " + fourthSent[floor(random(0, 3))] + " " + fifthSent[floor(random(0, 3))] + " " + sixthSent[floor(random(0, 3))] + " " + seventhSent[floor(random(0, 3))] + eigthSent[floor(random(0,3))];
-  let newSent3 = firstSent[floor(random(0, 3))] + " " + secondSent[floor(random(0, 3))] + " " + thirdSent[floor(random(0, 3))] + " " + fourthSent[floor(random(0, 3))] + " " + fifthSent[floor(random(0, 3))] + " " + sixthSent[floor(random(0, 3))] + " " + seventhSent[floor(random(0, 3))] + eigthSent[floor(random(0,3))];
-  let newSent4 = firstSent[floor(random(0, 3))] + " " + secondSent[floor(random(0, 3))] + " " + thirdSent[floor(random(0, 3))] + " " + fourthSent[floor(random(0, 3))] + " " + fifthSent[floor(random(0, 3))] + " " + sixthSent[floor(random(0, 3))] + " " + seventhSent[floor(random(0, 3))] + eigthSent[floor(random(0,3))];
-  let newSent5 = firstSent[floor(random(0, 3))] + " " + secondSent[floor(random(0, 3))] + " " + thirdSent[floor(random(0, 3))] + " " + fourthSent[floor(random(0, 3))] + " " + fifthSent[floor(random(0, 3))] + " " + sixthSent[floor(random(0, 3))] + " " + seventhSent[floor(random(0, 3))] + eigthSent[floor(random(0,3))];
 
-  //console.log(newSent);
+  newSent1 = intro[floor(random(0, 4))] + firstSent[floor(random(0, 4))] + " " + secondSent[floor(random(0, 4))] + " " + thirdSent[floor(random(0, 4))] + " " + fourthSent[floor(random(0, 4))] + " " + fifthSent[floor(random(0, 4))] + " " + sixthSent[floor(random(0, 4))] + " " + seventhSent[floor(random(0, 4))] + eigthSent[floor(random(0,4))];
+  newSent2 = intro[floor(random(0, 4))] + firstSent[floor(random(0, 4))] + " " + secondSent[floor(random(0, 4))] + " " + thirdSent[floor(random(0, 4))] + " " + fourthSent[floor(random(0, 4))] + " " + fifthSent[floor(random(0, 4))] + " " + sixthSent[floor(random(0, 4))] + " " + seventhSent[floor(random(0, 4))] + eigthSent[floor(random(0,4))];
+  newSent3 = intro[floor(random(0, 4))] + firstSent[floor(random(0, 4))] + " " + secondSent[floor(random(0, 4))] + " " + thirdSent[floor(random(0, 4))] + " " + fourthSent[floor(random(0, 4))] + " " + fifthSent[floor(random(0, 4))] + " " + sixthSent[floor(random(0, 4))] + " " + seventhSent[floor(random(0, 4))] + eigthSent[floor(random(0,4))];
+  newSent4 = intro[floor(random(0, 4))] + firstSent[floor(random(0, 4))] + " " + secondSent[floor(random(0, 4))] + " " + thirdSent[floor(random(0, 4))] + " " + fourthSent[floor(random(0, 4))] + " " + fifthSent[floor(random(0, 4))] + " " + sixthSent[floor(random(0, 4))] + " " + seventhSent[floor(random(0, 4))] + eigthSent[floor(random(0,4))];
+  newSent5 = intro[floor(random(0, 4))] + firstSent[floor(random(0, 4))] + " " + secondSent[floor(random(0, 4))] + " " + thirdSent[floor(random(0, 4))] + " " + fourthSent[floor(random(0, 4))] + " " + fifthSent[floor(random(0, 4))] + " " + sixthSent[floor(random(0, 4))] + " " + seventhSent[floor(random(0, 4))] + eigthSent[floor(random(0,4))];
+
+
+
 
   let newdiv1 = createDiv(newSent1);
-  let test = createP(" ");
-  let newdiv2 = createDiv(newSent2);
-  let test1 = createP(" ");
-  let newdiv3 = createDiv(newSent3);
-  let test2 = createP(" ");
-  let newdiv4 = createDiv(newSent4);
-  let test3 = createP(" ");
-  let newdiv5 = createDiv(newSent5);
+  button1 = createButton("Copy To Clipboard");
+  button1.mousePressed(button1F);
+  let test = createP("_______________________________________________________________________________________________________________________________________________");
 
+  let newdiv2 = createDiv(newSent2);
+  button2 = createButton("Copy To Clipboard");
+  button2.mousePressed(button2F);
+  let test1 = createP("_______________________________________________________________________________________________________________________________________________");
+
+  let newdiv3 = createDiv(newSent3);
+  button3 = createButton("Copy To Clipboard");
+  button3.mousePressed(button3F);
+  let test2 = createP("_______________________________________________________________________________________________________________________________________________");
+
+
+  let newdiv4 = createDiv(newSent4);
+  button4 = createButton("Copy To Clipboard");
+  button4.mousePressed(button4F);
+  let test3 = createP("_______________________________________________________________________________________________________________________________________________");
+
+
+  let newdiv5 = createDiv(newSent5);
+  button5 = createButton("Copy To Clipboard");
+  button5.mousePressed(button5F);
 }
 
 function draw() {
 //  background(51);
+}
 
-
+function button1F() {
+  copyToClipboard(newSent1);
+}
+function button2F() {
+  copyToClipboard(newSent2);
+}
+function button3F() {
+  copyToClipboard(newSent3);
+}
+function button4F() {
+  copyToClipboard(newSent4);
+}
+function button5F() {
+  copyToClipboard(newSent5);
 }
 
 function copyToClipboard(text) {
